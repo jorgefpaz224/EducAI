6594887d93c2d2ac6f34921425a69791672b8b48
RLNDT <cristyandavidmartinez@unitec.edu>
2024-11-18 15:10:59 -0600

    cambios aplicados

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 
 async function verificarEstudiante(correo, contrasena) {
-  const estudiante = await db('Estudiantes')
+  const estudiante = await db('Estudiante')
     .select('id_estudiante', 'PrimerNombre', 'Apellido')
     .where({ Correo: correo, contrasena: contrasena })
     .first();
@@ -10,7 +10,7 @@ async function verificarEstudiante(correo, contrasena) {
 }
 
 async function verificarMaestro(correo, contrasena) {
-    const maestro = await db('Maestros')
+    const maestro = await db('Maestro')
       .select('id_maestro', 'PrimerNombre', 'Apellido')
       .where({ Correo: correo, contrasena: contrasena })
       .first();
